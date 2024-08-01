a772f00f8491321fba6a4755db1a8599cf0dc9a1
dhirajpant <dhirajpant.ad@gmail.com>
2024-08-01 22:05:27 +0545

    added feedback system

--- app.py
+++ app.py
@@ -90,4 +90,10 @@ if 'predicted_place' in st.session_state:
         if st.button("Show in English"):
             st.session_state['description_language'] = "english"
             st.rerun()
+
+sentiment_mapping = ["one", "two", "three", "four", "five"]
+selected = st.feedback("stars")
+if selected is not None:
+    st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
+
 st.write("© 2024 Dharohar.All rights reserved")
